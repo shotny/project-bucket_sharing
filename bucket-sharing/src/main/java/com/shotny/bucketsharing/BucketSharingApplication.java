cee2612189a896aabc205104b764b5284db41e47
shotny <yeahh2483@gmail.com>
2023-03-06 19:28:41 +0900

    directory setting

---
+++ bucket-sharing/src/main/java/com/shotny/bucketsharing/BucketSharingApplication.java
@@ -0,0 +1,13 @@
+package com.shotny.bucketsharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BucketSharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BucketSharingApplication.class, args);
+	}
+
+}
