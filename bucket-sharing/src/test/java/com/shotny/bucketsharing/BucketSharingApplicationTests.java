6f52a40f47c6c35350503dea3220a2b2e47f7d4a
shotny <yeahh2483@gmail.com>
2023-03-06 19:02:40 +0900

    directory setting

---
+++ bucket-sharing/src/test/java/com/shotny/bucketsharing/BucketSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shotny.bucketsharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BucketSharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
