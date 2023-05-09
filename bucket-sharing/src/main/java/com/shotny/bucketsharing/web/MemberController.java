4e52bf41533807cab3e709c67584674b3514c820
shotny <yeahh2483@gmail.com>
2023-05-09 17:58:48 +0900

    feat: login

--- bucket-sharing/src/main/java/com/shotny/bucketsharing/web/MemberController.java
+++ bucket-sharing/src/main/java/com/shotny/bucketsharing/web/MemberController.java
@@ -1,5 +1,6 @@
 package com.shotny.bucketsharing.web;
 
+import com.shotny.bucketsharing.domain.member.Member;
 import com.shotny.bucketsharing.domain.member.dto.LoginDto;
 import com.shotny.bucketsharing.domain.member.dto.MemberSaveDto;
 import com.shotny.bucketsharing.service.BucketsService;
@@ -17,50 +18,97 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class MemberController {
 
+    // 1.회원가입  2.로그인  3.로그아웃
+
     private final MemberService memberService;
-    private final BucketsService bucketsService;
 
     @GetMapping("/join")
-    public String getJoinForm(Model model) {
+    public String getJoinForm() {
         return "join";
     }
 
-
-    @PostMapping("/nameCheck")
+    @PostMapping("/checkName")
     @ResponseBody
-    public boolean nameCheck(String name){
-        System.out.println(name);
-        return memberService.nameCheck(name);
+    public boolean checkName(String name){
+        System.out.println("nameCheck = " + name);
+        return memberService.isUsableName(name);
     }
 
     @PostMapping("/join")
     public String join(MemberSaveDto memberSaveDto) {
         System.out.println("name: "+ memberSaveDto.getName());
         System.out.println("password: "+ memberSaveDto.getPassword());
-        memberService.save(memberSaveDto);
+        memberService.saveMember(memberSaveDto);
         return "redirect:/v1/members/login";
     }
 
     @GetMapping("/login")
-    public String loginForm() {
-
+    public String getLoginForm() {
         return "login";
     }
 
-    @PostMapping("/login")
-    public String login(Model model, LoginDto loginDto, HttpServletResponse response) {
-        // 토큰발급 후 토큰에 저장된 유저정보로 유저 버킷 조회화면으로 넘어감
-//        memberService.login(loginDto);
-//        bucketsService.
-//        model.addAttribute("buckets",)
-        System.out.println("====== login ======");
-        System.out.println("name: "+loginDto.getName());
-        System.out.println("password: "+loginDto.getPassword());
+    @ResponseBody
+    @PostMapping("/test")
+    public String checkInfo(LoginDto dto) {
+        System.out.println("====== checkInfo ======");
+        System.out.println("name: " + dto.getName());
 
-        String createdToken = memberService.login(loginDto);
-        response.setHeader("authorization","bearer " + createdToken);
-        return "/buckets/buckets";
+        return memberService.matchLoginInfo2(dto);
     }
+    @PostMapping("/login")
+    public String login(LoginDto dto, Model model, HttpServletResponse response){
+
+        System.out.println("====== login ======");
+        System.out.println("name: "+dto.getName());
+
+        String token = memberService.login(dto);
+        response.setHeader("authorization", "bearer "+token);
+
+        Member member = memberService.findMemberByName(dto.getName()).get();
+        return "redirect:/v1/buckets/" + member.getId();
+    }
+
+
+
+
+//    @PostMapping("/logout/{memberId}")
+//    public String logout(@PathVariable Long memberId) {}
+
+
+//    private final BucketsService bucketsService;
+//
+//
+//    @GetMapping("/join")
+//    public String getJoinForm(Model model) {
+//        return "join";
+//    }
+
+
+//
+//    @GetMapping("/login")
+//    public String loginForm() {
+//
+//        return "login";
+//    }
+
+//    @PostMapping("/login")
+//    public String login(Model model, LoginDto loginDto, HttpServletResponse response) {
+//        // 토큰발급 후 토큰에 저장된 유저정보로 유저 버킷 조회화면으로 넘어감
+////        memberService.login(loginDto);
+////        bucketsService.
+////        model.addAttribute("buckets",)
+
+//        System.out.println("password: "+loginDto.getPassword());
+//
+//        if (memberService.login(loginDto) == "false") {
+//            return "false";
+//        }
+//        String createdToken = memberService.login(loginDto);
+//        response.setHeader("authorization", "bearer " + createdToken);
+//        Long memberId = memberService.findMemberId(loginDto.getName());
+//        return "redirect:/v1/buckets/" + memberId;
+//
+//    }
 
 //    @PostMapping("/login")
 //    public String login(Model model, LoginDto loginDto, HttpServletResponse response) {
@@ -85,19 +133,19 @@ public class MemberController {
 //        return "/buckets/buckets";
 //    }
 
-    @PostMapping("/inquiry")
-    @ResponseBody
-    public String inquiryMember(String name, String password) {
-        System.out.println("name = " + name);
-        System.out.println("password = " + password);
-        return memberService.inquiryMember(name, password);
-    }
+//    @PostMapping("/inquiry")
+//    @ResponseBody
+//    public String inquiryMember(String name, String password) {
+//        System.out.println("name = " + name);
+//        System.out.println("password = " + password);
+//        return memberService.inquiryMember(name, password);
+//    }
 
-    @PostMapping("/booleaninquiry")
-    @ResponseBody
-    public boolean booleaninquiryMember(String name, String password) {
-        System.out.println("name = " + name);
-        System.out.println("password = " + password);
-        return memberService.booleaninquiryMember(name, password);
-    }
+//    @PostMapping("/booleaninquiry")
+//    @ResponseBody
+//    public boolean booleaninquiryMember(String name, String password) {
+//        System.out.println("name = " + name);
+//        System.out.println("password = " + password);
+//        return memberService.booleaninquiryMember(name, password);
+//    }
 }
